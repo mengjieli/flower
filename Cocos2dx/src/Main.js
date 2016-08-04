875a0a1ffdd3c5ddd5332df91679227c932b60d1
MengJieLi <279037995@qq.com>
2016-08-04 18:37:22 +0800

    完成变形编辑器

--- Cocos2dx/src/Main.js
+++ Cocos2dx/src/Main.js
@@ -16,8 +16,27 @@ var Main = function () {
         value: function ready() {
             trace("ready");
 
+            //var px1 = 50;
+            //var py1 = 0;
+            //var px2 = -50;
+            //var py2 = 50 * Math.sqrt(3);
+            //
+            //var r = this.getR(px1, py1, px2, py2);
+            //console.log(r);
+            //return;
+
             var preloading = new PreLoading();
             preloading.addListener(flower.Event.COMPLETE, this.loadThemeComplete, this);
+        }
+    }, {
+        key: "getR",
+        value: function getR(px1, py1, px2, py2) {
+            var cos = (px1 * px2 + py1 * py2) / (Math.sqrt(px1 * px1 + py1 * py1) * Math.sqrt(px2 * px2 + py2 * py2));
+            var sin = (px1 * py2 - py1 * px2) / (Math.sqrt(px1 * px1 + py1 * py1) * Math.sqrt(px2 * px2 + py2 * py2));
+            console.log(sin, cos);
+            var r = Math.acos(cos);
+            r = r * 180 / Math.PI;
+            return r;
         }
     }, {
         key: "loadThemeComplete",
